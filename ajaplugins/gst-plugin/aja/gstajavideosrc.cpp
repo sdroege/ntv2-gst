1542ec959c3a1a047946e296927607842aa1089f
Sebastian Dröge <sebastian@centricular.com>
2020-11-10 10:53:48 +0200

    Remove bitrotten HEVC capture code
    This couldn't have worked for a long time and was disabled in the build
    anyway.

--- ajaplugins/gst-plugin/aja/gstajavideosrc.cpp
+++ ajaplugins/gst-plugin/aja/gstajavideosrc.cpp
@@ -83,8 +83,8 @@ aja_capture_video_frame_clear (void *data)
 {
   AjaCaptureVideoFrame *frame = (AjaCaptureVideoFrame *) data;
 
-  if ((frame->video_buff) && (frame->video_src->input) && (frame->video_src->input->ntv2AVHevc))
-    frame->video_src->input->ntv2AVHevc->ReleaseVideoBuffer (frame->video_buff);
+  if ((frame->video_buff) && (frame->video_src->input) && (frame->video_src->input->ntv2AV))
+    frame->video_src->input->ntv2AV->ReleaseVideoBuffer (frame->video_buff);
   memset(frame, 0, sizeof (*frame));
 }
 
@@ -329,7 +329,7 @@ gst_aja_video_src_set_property (GObject * object, guint property_id,
 
     case PROP_TIMECODE_MODE:
       src->timecode_mode = (GstAjaTimecodeMode) g_value_get_enum (value);
-      if (src->input && src->input->ntv2AVHevc) {
+      if (src->input && src->input->ntv2AV) {
         NTV2TCIndex timecode_mode;
 
         switch (src->timecode_mode) {
@@ -357,7 +357,7 @@ gst_aja_video_src_set_property (GObject * object, guint property_id,
             g_assert_not_reached ();
             break;
         }
-        src->input->ntv2AVHevc->UpdateTimecodeIndex(timecode_mode);
+        src->input->ntv2AV->UpdateTimecodeIndex(timecode_mode);
       }
 
       break;
@@ -593,7 +593,7 @@ gst_aja_video_src_open (GstAjaVideoSrc * src)
 
   src->input =
       gst_aja_acquire_input (src->device_identifier, src->input_channel,
-      GST_ELEMENT_CAST (src), FALSE, FALSE);
+      GST_ELEMENT_CAST (src), FALSE);
   if (!src->input) {
     GST_ERROR_OBJECT (src, "Failed to acquire input");
     return FALSE;
@@ -606,7 +606,7 @@ gst_aja_video_src_open (GstAjaVideoSrc * src)
   src->input->mode = mode;
   src->input->start_streams = gst_aja_video_src_start_streams;
 
-  status = src->input->ntv2AVHevc->Open ();
+  status = src->input->ntv2AV->Open ();
   if (!AJA_SUCCESS (status)) {
     GST_ERROR_OBJECT (src, "Failed to open input");
     g_mutex_unlock (&src->input->lock);
@@ -665,14 +665,12 @@ gst_aja_video_src_open (GstAjaVideoSrc * src)
       break;
   }
 
-  status = src->input->ntv2AVHevc->Init (src->input->mode->videoPreset,
-      src->input->mode->videoFormat,
+  status = src->input->ntv2AV->Init (src->input->mode->videoFormat,
       input_source,
       src->input->mode->bitDepth,
       src->input->mode->is422,
       false,
-      false,
-      src->sdi_input_mode, timecode_mode, false, src->output_cc ? true : false,
+      src->sdi_input_mode, timecode_mode, src->output_cc ? true : false,
       src->passthrough ? true : false, network_mode, src->network_config);
   if (!AJA_SUCCESS (status)) {
     GST_ERROR_OBJECT (src, "Failed to initialize input");
@@ -693,11 +691,11 @@ gst_aja_video_src_close (GstAjaVideoSrc * src)
   if (src->input) {
     g_mutex_lock (&src->input->lock);
 
-    if (src->input->ntv2AVHevc) {
-      src->input->ntv2AVHevc->Quit ();
-      src->input->ntv2AVHevc->Close ();
-      delete src->input->ntv2AVHevc;
-      src->input->ntv2AVHevc = NULL;
+    if (src->input->ntv2AV) {
+      src->input->ntv2AV->Quit ();
+      src->input->ntv2AV->Close ();
+      delete src->input->ntv2AV;
+      src->input->ntv2AV = NULL;
       GST_DEBUG_OBJECT (src, "shut down ntv2HEVC");
     }
 
@@ -721,9 +719,9 @@ gst_aja_video_src_stop (GstAjaVideoSrc * src)
 
   if (src->input && src->input->video_enabled) {
     g_mutex_lock (&src->input->lock);
-    src->input->ntv2AVHevc->Quit ();
+    src->input->ntv2AV->Quit ();
     src->input->video_enabled = FALSE;
-    src->input->ntv2AVHevc->SetCallback (VIDEO_CALLBACK, 0, 0);
+    src->input->ntv2AV->SetCallback (VIDEO_CALLBACK, 0, 0);
     g_mutex_unlock (&src->input->lock);
   }
 
@@ -768,9 +766,9 @@ gst_aja_video_src_start_streams (GstElement * element)
     src->next_time_mapping.den = 1;
     g_mutex_unlock (&src->lock);
 
-    if (src->input->ntv2AVHevc) {
+    if (src->input->ntv2AV) {
       src->input->started = TRUE;
-      src->input->ntv2AVHevc->Run ();
+      src->input->ntv2AV->Run ();
     }
   } else {
     GST_DEBUG_OBJECT (src, "Not starting streams yet");
@@ -793,7 +791,7 @@ gst_aja_video_src_change_state (GstElement * element, GstStateChange transition)
 
     case GST_STATE_CHANGE_READY_TO_PAUSED:
       g_mutex_lock (&src->input->lock);
-      src->input->ntv2AVHevc->SetCallback (VIDEO_CALLBACK,
+      src->input->ntv2AV->SetCallback (VIDEO_CALLBACK,
           &gst_aja_video_src_video_callback, src);
       g_mutex_unlock (&src->input->lock);
       src->flushing = FALSE;
@@ -996,7 +994,7 @@ gst_aja_video_src_got_frame (GstAjaVideoSrc * src, AjaVideoBuff * videoBuff)
 
   // Check if we have no signal
   if (videoBuff && !videoBuff->haveSignal) {
-    src->input->ntv2AVHevc->ReleaseVideoBuffer (videoBuff);
+    src->input->ntv2AV->ReleaseVideoBuffer (videoBuff);
     videoBuff = NULL;
   }
 
@@ -1058,7 +1056,7 @@ gst_aja_video_src_got_frame (GstAjaVideoSrc * src, AjaVideoBuff * videoBuff)
         "Skipping frame as requested: %" GST_TIME_FORMAT " < %" GST_TIME_FORMAT,
         GST_TIME_ARGS (stream_time),
         GST_TIME_ARGS (src->skip_first_time + src->first_time));
-    src->input->ntv2AVHevc->ReleaseVideoBuffer (videoBuff);
+    src->input->ntv2AV->ReleaseVideoBuffer (videoBuff);
     return;
   }
 
@@ -1143,7 +1141,7 @@ gst_aja_video_src_got_frame (GstAjaVideoSrc * src, AjaVideoBuff * videoBuff)
     g_mutex_unlock (&src->lock);
   } else {
     g_mutex_unlock (&src->lock);
-    src->input->ntv2AVHevc->ReleaseVideoBuffer (videoBuff);
+    src->input->ntv2AV->ReleaseVideoBuffer (videoBuff);
   }
 }
 
